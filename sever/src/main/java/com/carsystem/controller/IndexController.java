811715a9da9f1fb87996596df3a080b27cb9a9c5
vfru <mailforvfru@gmail.com>
2023-03-10 02:14:16 +0800

    12

--- sever/src/main/java/com/carsystem/controller/IndexController.java
+++ sever/src/main/java/com/carsystem/controller/IndexController.java
@@ -3,21 +3,26 @@ package com.carsystem.controller;
 import com.carsystem.pojo.User;
 import com.carsystem.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
 
-@Controller
+@RestController
 public class IndexController {
     @Autowired
     private UserService userService;
 
+    @RequestMapping("/{id}")
+    public String getByid(@PathVariable Integer id) {
+        System.out.println(id);
+        return "hello" + id;
+    }
 
-
-    @GetMapping("/list")
-    public List<User> list(){
+    @RequestMapping("/users")
+    public List<User> list() {
         return userService.list();
     }
 
