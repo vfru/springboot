a4b0172e8d4cffc3a9716d2fd6380f9e60c90dd0
vfru <mailforvfru@gmail.com>
2023-03-10 17:14:52 +0800

    12

--- sever/src/main/java/com/carsystem/service/UserService.java
+++
@@ -1,7 +0,0 @@
-package com.carsystem.service;
-
-import com.baomidou.mybatisplus.extension.service.IService;
-import com.carsystem.pojo.User;
-
-public interface UserService  extends IService<User> {
-}
