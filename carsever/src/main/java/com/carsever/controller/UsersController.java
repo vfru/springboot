e10b4b13466b8e5245ebf895d14bbb3156ba0f05
vfru <mailforvfru@gmail.com>
2023-03-13 17:24:00 +0800

    313

--- carsever/src/main/java/com/carsever/controller/UsersController.java
+++ carsever/src/main/java/com/carsever/controller/UsersController.java
@@ -3,17 +3,15 @@ package com.carsever.controller;
 
 import com.carsever.pojo.Users;
 import com.carsever.service.IUsersService;
+import org.apache.catalina.User;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 /**
  * <p>
- *  前端控制器
+ * 前端控制器
  * </p>
  *
  * @author carSystem
@@ -26,10 +24,39 @@ public class UsersController {
     @Autowired
     private IUsersService usersService;
 
+    //得到用户的全部列表
     @GetMapping("/list")
-    public List<Users> list(){
+    public List<Users> list() {
         return usersService.list();
     }
+
+    //拉黑
+    @PatchMapping("/{id}")
+    public boolean block(@PathVariable Integer id) {
+        Users blockUser = usersService.getById(id);
+        blockUser.setBlock(1);
+        return usersService.updateById(blockUser);
+    }
+
+    //删除
+    @DeleteMapping("/{id}")
+    public boolean delete(@PathVariable Integer id) {
+        return usersService.removeById(id);
+    }
+
+    //增加
+    @PostMapping
+    public boolean save(@RequestBody Users user){
+        return usersService.save(user);
+    }
+
+    //修改
+    @PatchMapping
+    public boolean update(@RequestBody Users user){
+       return usersService.updateById(user);
+    }
+
+
 
 }
 
